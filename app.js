c6af3e0bf26c812995c752ee087a27897148e6ef
Maria Pereira <mariaaaa229@gmail.com>
2025-11-26 16:49:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Seja bem-vindo ao nosso site, jovem padawan!");
+function enviarWhatsApp() {
+    const nome = document.getElementById("nome").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    const texto = `Olá, meu nome é ${nome}. Mensagem: ${mensagem}`;
+    const url = `https://wa.me/5599999999999?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, "_blank");
+}
