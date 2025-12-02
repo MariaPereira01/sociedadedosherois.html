dbd436f36ac742a1070f2be240b61dd3e27cf427
Maria Pereira <mariaaaa229@gmail.com>
2025-12-02 16:44:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-function enviarWhatsApp() {
+function enviarZapZap() {
     const nome = document.getElementById("nome").value;
     const mensagem = document.getElementById("mensagem").value;
 
-    const texto = `Olá, meu nome é ${nome}. Mensagem: ${mensagem}`;
-    const url = `https://wa.me/5599999999999?text=${encodeURIComponent(texto)}`;
+    const texto = `Olá, meu nome é ${nome}. Gostaria de conversar sobre seu site. ${mensagem}`;
+    const url = `https://wa.me/5581997391760?text=${encodeURIComponent(texto)}`;
 
     window.open(url, "_blank");
 }
-
 
